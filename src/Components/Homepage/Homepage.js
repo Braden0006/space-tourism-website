c6c803a74ac7666419f74312044c8940b709fa79
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-17 17:23:32 -0600

    Applied fonts to text in "Homepage"

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -7,7 +7,7 @@ export default function Homepage() {
     <main className="homepage">
       <div className="homepage__info">
           <h5 className="homepage__info__title-small">SO, YOU WANT TO TRAVEL TO</h5>
-          <h1 className="homepage__info__title">SPACE</h1>
+          <span className="homepage__info__title">SPACE</span>
           <p className="homepage__info__para">
             Let’s face it; if you want to go to space, you might as well genuinely
             go to outer space and not hover kind of on the edge of it. Well sit
