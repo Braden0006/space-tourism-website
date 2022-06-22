238081abfaa7ceb76debe3f9f781a70a90e42677
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-22 15:26:37 -0600

    Finished styling for "MarsDestination" component

--- src/Components/MarsDestination/MarsDestination.js
+++ src/Components/MarsDestination/MarsDestination.js
@@ -4,24 +4,24 @@ import "./MarsDestination.css";
 
 export default function MarsDestination() {
   return (
-    <section className="moon">
-      <h1 className="moon__title">MARS</h1>
-      <p className="moon__description">
+    <section className="mars">
+      <h1 className="mars__title">MARS</h1>
+      <p className="mars__description">
         Don’t forget to pack your hiking boots. You’ll need them to tackle
         Olympus Mons, the tallest planetary mountain in our solar system. It’s
         two and a half times the size of Everest!
       </p>
 
-      <span className="moon__line"></span>
+      <span className="mars__line"></span>
 
-      <div className="moon__travel">
-        <div className="moon__travel__distance">
-          <div className="moon__travel__distance__heading">AVG. DISTANCE</div>
-          <div className="moon__travel__distance__number">225 mil. KM</div>
+      <div className="mars__travel">
+        <div className="mars__travel__distance">
+          <div className="mars__travel__distance__heading">AVG. DISTANCE</div>
+          <div className="mars__travel__distance__number">225 mil. KM</div>
         </div>
-        <div className="moon__travel__time">
-          <div className="moon__travel__time__heading">EST. TRAVEL TIME</div>
-          <div className="moon__travel__time__number">9 months</div>
+        <div className="mars__travel__time">
+          <div className="mars__travel__time__heading">EST. TRAVEL TIME</div>
+          <div className="mars__travel__time__number">9 months</div>
         </div>
       </div>
     </section>
