a996c99f980ca91ca1f19dc264d3616330c793d3
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-22 16:58:59 -0600

    Created "CrewA" component

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ function App() {
             </Carousel>
           }
         />
+
+        <Route path="/crew" />
       </Routes>
     </>
   );
---
+++ src/Components/CrewA/CrewA.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CrewA() {
+  return (
+    <section className="crew-a">
+      <h5>COMMANDER</h5>
+      <h4>DOUGLAS HURLEY</h4>
+      <p>
+        Douglas Gerald Hurley is an American engineer, former Marine Corps pilot
+        and former NASA astronaut. He launched into space for the third time as
+        commander of Crew Dragon Demo-2.
+      </p>
+    </section>
+  );
+}
