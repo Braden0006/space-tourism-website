94e1941ab00cb29bbee44dd3b3279bfe7c77199e
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-22 15:23:21 -0600

    Created mixin for the planet destinations

--- src/Components/MarsDestination/MarsDestination.js
+++ src/Components/MarsDestination/MarsDestination.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import './MarsDestination.css'
+import "./MarsDestination.css";
 
 export default function MarsDestination() {
   return (
-    <div className='mars'>
-    <img src="/Images/image-mars.webp" alt="" className="mars__img" />
-    </div>
-  )
+    <section className="moon">
+      <h1 className="moon__title">MARS</h1>
+      <p className="moon__description">
+        Don’t forget to pack your hiking boots. You’ll need them to tackle
+        Olympus Mons, the tallest planetary mountain in our solar system. It’s
+        two and a half times the size of Everest!
+      </p>
+
+      <span className="moon__line"></span>
+
+      <div className="moon__travel">
+        <div className="moon__travel__distance">
+          <div className="moon__travel__distance__heading">AVG. DISTANCE</div>
+          <div className="moon__travel__distance__number">225 mil. KM</div>
+        </div>
+        <div className="moon__travel__time">
+          <div className="moon__travel__time__heading">EST. TRAVEL TIME</div>
+          <div className="moon__travel__time__number">9 months</div>
+        </div>
+      </div>
+    </section>
+  );
 }
