2490dd003fabe62de754da7746f5ce86c6f7513b
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-25 17:39:42 -0600

    Added array of objects of images to display in "TechnologyCarousel"

--- src/Components/TechnologyCarousel/TechnologyCarousel.js
+++ src/Components/TechnologyCarousel/TechnologyCarousel.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
+import launch from "../Images/TechnologyImg/image-launch-vehicle-portrait.jpg";
+import space from "../Images/TechnologyImg/image-space-capsule-portrait.jpg";
+import spaceport from "../Images/TechnologyImg/image-spaceport-portrait.jpg";
+
 import "./TechnologyCarousel.css";
 
 export default function TechnologyCarousel({ children }) {
   const [technologyActiveIndex, setTechnologyActiveIndex] = useState(0);
 
+  // updates the index on which component is active
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
       newIndex = 0;
@@ -15,9 +20,37 @@ export default function TechnologyCarousel({ children }) {
     setTechnologyActiveIndex(newIndex);
   };
 
+  // An array of objects of images to display depending on the index
+  const images = [
+    { id: 0, image: launch },
+    { id: 1, image: space },
+    { id: 2, image: spaceport },
+  ];
+
   return (
     <main className="technology-main">
       <div className="technology-main__carousel">
+        <h5>
+          <span>03</span>SPACE LAUNCH 101
+        </h5>
+
+        <div className="technology-main__carousel__img-container">
+          <div className="technology-main__carousel__img">
+            <img
+              className="technology-main__carousel__img-image"
+              src={images
+                .map((image) => {
+                  if (image.id === technologyActiveIndex) {
+                    return image.image;
+                  }
+                  return null;
+                })
+                .join(" ")}
+              alt="Images of the technology used"
+            />
+          </div>
+        </div>
+
         <div className="technology-main__carousel__buttons">
           {React.Children.map(children, (child, index) => {
             return (
