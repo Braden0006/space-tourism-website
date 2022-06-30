d8d8dd68fedc310a9713f2bd1cc925844c35e20f
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-06-30 17:38:52 -0600

    "MarsDestination" is responsive on all devices

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
           path="/destinations"
           element={
             <Carousel>
-              <CarouselItem activeIndex>
+              <CarouselItem>
                 <MoonDestination />
               </CarouselItem>
 
